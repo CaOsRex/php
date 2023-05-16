71d996d922206408ddea07e97072f793fedbac11
CaOsRex <camilocaballero9907@gmail.com>
2023-05-16 11:46:38 -0500

    feat ( firts commit ): :rocket: primerar codeadas de php
    primeros codigos usando el php

---
+++ intro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>intro</title>
+    </head>
+    <body>
+    <?php
+    echo "<h1>titulo con html generado desde php</h1>";
+    echo phpversion();
+    /* echo phpinfo(); */
+    
+    /* 1. Variables */
+    /* $variable = "helloooooow dudeeee";
+    echo $variable;*/
+
+    /* 2. Errores */
+    /* 2.1 Parse error: syntax error */
+    $variable = "helloooooow dudeeee";
+    echo $variable;
+
+    /* 2.2 Warning: */
+    echo $variablehfdstgh;
+    echo "esto si se debe de ejecutar";
+
+    /* 2.3 Fatal error: */
+    kfdniohfn;
+
+    ?>  
+    
+    </body>
+</html>
+
