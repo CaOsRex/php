ccb5bf8e89920c15c737b5729b33e3e6cdeede33
CaOsRex <camilocaballero9907@gmail.com>
2023-06-02 12:27:20 -0500

    bien

---
+++ if_case_loop_arrays.php
@@ -0,0 +1,55 @@
+<?php
+/* 1.   IF CONDICIONAL */
+/* $salario = 900000;
+$No_Subsidio_Msg = "Usted (NO) tiene derecho a subsidio";
+$Si_Subsidio_Msg = "Usted (SI) tiene derecho a subsidio";
+if ($salario>= 5000000) {
+    echo $No_Subsidio_Msg;
+}
+elseif ($salario>= 3000000) {
+    echo $No_Subsidio_Msg;
+}
+elseif ($salario>= 1000000) {
+    echo $Si_Subsidio_Msg;
+}
+else {
+    echo "Usted tiene derecho a un subsidio de vivienda";
+} */
+
+/* $temperatura = 15;
+if ($temperatura < 0 || $temperatura > 30) {
+    echo "El clima no es favorable";
+}
+else {
+    echo "El clima es bueno";
+} */
+
+/* --------------------------------------------------------------- */
+
+/* 2.   SWITCH*/
+/*     $hora = "03:00 P.M";
+    switch ($hora) {
+        case '06:00 A.M':
+            echo "Camper, tienes Software Skill";
+            break;
+        case '08:00 A.M':
+            echo "Camper, tienes Break, Pausa activa";
+            break;
+        case '10:00 A.M':
+            echo "Camper, tienes Break, Pausa activa";
+            break;
+        case '12:00 A.M':
+                echo "Camper, tienes Break, Pausa activa";
+                break;
+        case '02:00 P.M':
+                echo "Camper, tienes Break, Pausa activa";
+                break;
+        default:
+            echo "Camper, Descansa";
+            break;
+    } */
+
+/* --------------------------------------------------------------- */
+
+
+?>
