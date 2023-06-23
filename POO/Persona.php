0289f93b3872337cafbcf45f59a19f310ee45176
CaOsRex <camilocaballero9907@gmail.com>
2023-06-23 11:39:41 -0500

    no funciona :(

--- POO/Persona.php
+++ POO/Persona.php
@@ -1,47 +1,61 @@
 <?php
-class Empleado {
+
+class Persona {
     protected $nombre;
     protected $apellido;
     protected $email;
     protected $telefono;
-    protected $codigo;
-    protected $departamento;
-    public function __construct($nombre, $apellido, $email, $telefono, $codigo, $departamento)
+
+    public function __construct($nombre, $apellido, $email, $telefono)
     {
         $this->nombre = $nombre;
         $this->apellido = $apellido;
         $this->email = $email;
         $this->telefono = $telefono;
+    }
+
+    public function mostrarInformacion()
+    {
+        echo "Nombre: " . $this->nombre . " " . $this->apellido . "Email: " . $this->email;
+    }
+}
+
+class Empleado extends Persona{
+    
+    protected $codigo;
+    protected $departamento;
+    
+    public function __construct($nombre, $apellido, $email, $telefono, $codigo, $departamento)
+    {
+        parent::__construct($nombre, $apellido, $email, $telefono);
         $this->codigo = $codigo;
         $this->departamento = $departamento;
     }
-
-    public function mostrarInformacion()
-    {
-        echo "Nombre: " . $this->nombre . " " . $this->apellido . "Email: " . $this->email;
-    }
 }
-class Proveedor {
-    protected $nombre;
-    protected $apellido;
-    protected $email;
-    protected $telefono;
+class Proveedor extends Persona{
+
     protected $empresa;
+    
     public function __construct($nombre, $apellido, $email, $telefono, $empresa)
     {
-        $this->nombre = $nombre;
-        $this->apellido = $apellido;
-        $this->email = $email;
-        $this->telefono = $telefono;
+        parent::__construct($nombre, $apellido, $email, $telefono);
         $this->empresa = $empresa;
-    }
-
-    public function mostrarInformacion()
-    {
-        echo "Nombre: " . $this->nombre . " " . $this->apellido . "Email: " . $this->email;
     }
 }
 
-$empleado = new Empleado('Sebastian Daniel' , 'Campos')
+$empleado = new Empleado('Sebastian Daniel' , 'Campos', 'sebastian@campuslands.com');
+$proveedor = new Proveedor('Josue', 'Perez', 'josue@campuslands.com');
+
+var_dump($empleado);
+echo "<br>";
+echo "<br>";
+var_dump($proveedor);
+echo "<br>";
+echo "<br>";
+
+$empleado->mostrarInformacion();
+echo "<br>";
+echo "<br>";
+$proveedor->mostrarInformacion();
 
 ?>
